b4d41d4db757b6c509a9d066cdcf4d5a323c2fcd
LuisCarlosFonseca98 <luiscarfonseca@hotmail.com>
2022-04-30 20:58:34 +0100

    PrototypeDevUpdated
    ->Simple AI
    ->Select and drag spacheship
    ->Move the spaceships(has a bug that needs to be fixed)

---
+++ src/CaptureTheFlagMastermind/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    void Start()
+    {
+        UnitSelections.Instance.unitList.Add(this.gameObject);
+    }
+
+    void onDestroy()
+    {
+        UnitSelections.Instance.unitList.Remove(this.gameObject);
+    }
+}
---
+++ src/CaptureTheFlagMastermind/Assets/Scripts/Units/UnitSelections.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelections : MonoBehaviour
+{
+
+    public List<GameObject> unitList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+
+    private static UnitSelections _instance;
+    public static UnitSelections Instance { get { return _instance;}}
+
+
+    public void Awake()
+    {
+        //if an instance of this already exists and it isnt this one
+        if(_instance != null && _instance != this)
+        {
+            //we destroy this instance
+            Destroy(this.gameObject);
+        }
+        else
+        {   
+            //make this the instance
+            _instance = this;
+        }
+    }
+
+    public void ClickSelect(GameObject unitToAdd)
+    {
+        DeselectAll();
+        unitsSelected.Add(unitToAdd);
+        unitToAdd.transform.GetChild(0).gameObject.SetActive(true);
+        unitToAdd.GetComponent<SpaceshipMov>().enabled = true;
+    }
+
+    public void ShiftClickSelect(GameObject unitToAdd)
+    {
+        if (!unitsSelected.Contains(unitToAdd))
+        {
+            unitsSelected.Add(unitToAdd);
+            unitToAdd.transform.GetChild(0).gameObject.SetActive(true);
+            unitToAdd.GetComponent<SpaceshipMov>().enabled = true;
+
+        }
+        else
+        {
+            unitToAdd.GetComponent<SpaceshipMov>().enabled = false;
+            unitToAdd.transform.GetChild(0).gameObject.SetActive(true);
+            unitsSelected.Remove(unitToAdd);
+        }
+    }
+
+    public void DragSelect(GameObject unitToAdd)
+    {
+        if (!unitsSelected.Contains(unitToAdd))
+        {
+            unitsSelected.Add(unitToAdd);
+            unitToAdd.transform.GetChild(0).gameObject.SetActive(true);
+            unitToAdd.GetComponent<SpaceshipMov>().enabled = true;
+        }
+    }
+
+    public void DeselectAll()
+    {
+        foreach(var unit in unitsSelected)
+        {
+            unit.GetComponent<SpaceshipMov>().enabled = false;
+            unit.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        unitsSelected.Clear();
+    }
+
+    public void Deselect(GameObject unitToDeselect)
+    {
+
+    }
+
+
+
+
+
+}
